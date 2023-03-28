0bcb4f18ec58bc4b5a897fb5faccadeff8580af9
Serly Teymoorian <98438828+SerlyTeymoorian@users.noreply.github.com>
2023-03-27 18:43:47 -0700

    RampController script added

---
+++ Assets/Scripts/RampController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RampController : MonoBehaviour
+{
+    public float horForce; 
+    public float verForce;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Ramp")
+        {
+            collider.GetComponent<Rigidbody2D>().velocity = new Vector2(horForce, verForce); 
+        }
+    } 
+}
