ee39aebea16bfac645a31c9c8aba27a550e93a1e
Serly Teymoorian <98438828+SerlyTeymoorian@users.noreply.github.com>
2023-03-29 17:00:50 -0700

    added ResetLevel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null; 
-    public int maxLevel = 4;
+    public int maxLevel = 15;
     public int currentLevel = 1; 
 
     void Awake()
@@ -23,6 +23,14 @@ public class GameManager : MonoBehaviour
 
         //Don't destroy the object when loading the scenes to not lose information about the game (like levels or score)
         DontDestroyOnLoad(gameObject);
+    }
+    
+    //function: Reset
+    //purpose: Restart the current level 
+    public void ResetLevel()
+    {
+        //load the scene for the same level again 
+        SceneManager.LoadScene("Level" + currentLevel); 
     }
 
     //function: IncreaseLevel 
