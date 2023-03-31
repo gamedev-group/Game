46b6940b48ecd35bfec720c3a623a802bd3ad84b
SerlyTeymoorian <teymoorian@cpp.edu>
2023-03-31 11:50:53 -0700

    added volume slider

---
+++ Assets/Scripts/MixerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio; 
+
+public class MixerController : MonoBehaviour
+{
+    public AudioMixer audioMixer; 
+
+    void Awake()
+    {
+       // audioMixer.SetFloat("MasterVolume", Mathf.Log10(0.0005f) * 20); 
+    }
+
+    //function: SetVolume
+    //purpose: set the volume of the music based on the slider 
+    public void SetVolume(float sliderValue)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20); 
+    }
+}
