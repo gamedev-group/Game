1ccceb516223e3efea120c17400d2eb6777b4d17
SerlyTeymoorian <teymoorian@cpp.edu>
2023-04-05 12:22:11 -0700

    little change to ramp

--- Assets/Scripts/RampController.cs
+++ Assets/Scripts/RampController.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class RampController : MonoBehaviour
 {
-    public float speedBoost = 20f;
+    public float speedBoost = 2f;
 
-    void OnCollisionStay2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        float horizontalVelocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity.x;
-        print("Horizantal:" + horizontalVelocity);
-        float verticalVelocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity.y;
-        print("Vertical:" + verticalVelocity);
-        collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(speedBoost * horizontalVelocity, verticalVelocity * 2);
-
+        if(collision.gameObject.tag == "Player")
+        {
+            float horizontalVelocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity.x;
+            float verticalVelocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity.y;
+            print("Hor velocity is: " + horizontalVelocity);
+            print("Ver velcocity is: " + verticalVelocity);
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalVelocity * 40, speedBoost);
+        }
 
     }
 
