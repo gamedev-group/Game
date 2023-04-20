06298b6e901e8c7c69ba77f7c7b7e7f43d5c0f95
SerlyTeymoorian <teymoorian@cpp.edu>
2023-04-19 19:04:20 -0700

    magnet updated

--- Assets/Scripts/MagnetController.cs
+++ Assets/Scripts/MagnetController.cs
@@ -6,17 +6,16 @@ public class MagnetController : MonoBehaviour
 {
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.gameObject.TryGetComponent<Block>(out Block block) && GameManager.instance.hasMagnet == true && ((Time.time - ItemPlacer.magnetStartTime) < 3f))
+        if(collision.gameObject.TryGetComponent<Block>(out Block block) && GameManager.instance.hasMagnet == true && (Mathf.Abs(transform.parent.position.x - collision.gameObject.transform.position.x) > 1.5))
         {
-            print(Time.time - ItemPlacer.magnetStartTime); 
-
             block.SetTarget(transform.parent.position, true); 
+            print("Yesss");
         }
-        else if(collision.gameObject.TryGetComponent<Block>(out Block block2) && GameManager.instance.hasMagnet == true && ((Time.time - ItemPlacer.magnetStartTime) >= 3f))
+        else if(collision.gameObject.TryGetComponent<Block>(out Block block2) && GameManager.instance.hasMagnet == true &&(Mathf.Abs(transform.parent.position.x - collision.gameObject.transform.position.x) <= 1.5))
         {
             GameManager.instance.hasMagnet = false; 
             block2.SetHasTarget(false); 
-            print(Time.time - ItemPlacer.magnetStartTime); 
+            print("nooooo"); 
         }
         else if(collision.gameObject.TryGetComponent<Block>(out Block block3))
         {
@@ -24,3 +23,5 @@ public class MagnetController : MonoBehaviour
         }
     }
 }
+
+//((Time.time - ItemPlacer.magnetStartTime) < 3f
