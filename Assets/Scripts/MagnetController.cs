8954fd5a87adb996709895f6ae376b6c5e4f89a4
SerlyTeymoorian <teymoorian@cpp.edu>
2023-04-19 19:06:19 -0700

    magnet script updated

--- Assets/Scripts/MagnetController.cs
+++ Assets/Scripts/MagnetController.cs
@@ -9,13 +9,11 @@ public class MagnetController : MonoBehaviour
         if(collision.gameObject.TryGetComponent<Block>(out Block block) && GameManager.instance.hasMagnet == true && (Mathf.Abs(transform.parent.position.x - collision.gameObject.transform.position.x) > 1.5))
         {
             block.SetTarget(transform.parent.position, true); 
-            print("Yesss");
         }
         else if(collision.gameObject.TryGetComponent<Block>(out Block block2) && GameManager.instance.hasMagnet == true &&(Mathf.Abs(transform.parent.position.x - collision.gameObject.transform.position.x) <= 1.5))
         {
             GameManager.instance.hasMagnet = false; 
             block2.SetHasTarget(false); 
-            print("nooooo"); 
         }
         else if(collision.gameObject.TryGetComponent<Block>(out Block block3))
         {
@@ -23,5 +21,3 @@ public class MagnetController : MonoBehaviour
         }
     }
 }
-
-//((Time.time - ItemPlacer.magnetStartTime) < 3f
