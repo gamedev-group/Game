95eba373a0fdd378bfc71d6691142fa987012f20
SerlyTeymoorian <teymoorian@cpp.edu>
2023-04-25 23:07:39 -0700

    minor change to magnet

--- Assets/Scripts/MagnetController.cs
+++ Assets/Scripts/MagnetController.cs
@@ -6,16 +6,7 @@ public class MagnetController : MonoBehaviour
 {
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.gameObject.TryGetComponent<Block>(out Block block) && GameManager.instance.hasMagnet == true && (Mathf.Abs(transform.parent.position.x - collision.gameObject.transform.position.x) > 1))
-        {
-            //the target position is the position of the player 
-            block.SetTarget(transform.parent.position, true); 
-            //sound effect of magnet
-            SoundManagerController.PlaySoundEffect("magnet"); 
-
-            print("hello"); 
-        }
-        else if(collision.gameObject.TryGetComponent<Block>(out Block block1) && GameManager.instance.hasMagnet == true && (Mathf.Abs(transform.parent.position.x - collision.gameObject.transform.position.x) > 1) )
+        if(collision.gameObject.TryGetComponent<Block>(out Block block1) && GameManager.instance.hasMagnet == true && (Mathf.Abs(transform.parent.position.x - collision.gameObject.transform.position.x) < 1.4) &&  (Mathf.Abs(transform.parent.position.y - collision.gameObject.transform.position.y) < 0.55))
         {
             GameManager.instance.hasMagnet = false; 
             block1.SetHasTarget(false); 
@@ -25,6 +16,15 @@ public class MagnetController : MonoBehaviour
             }
 
             print("bye"); 
+        }
+        else if(collision.gameObject.TryGetComponent<Block>(out Block block) && GameManager.instance.hasMagnet == true )
+        {
+            //the target position is the position of the player 
+            block.SetTarget(transform.parent.position, true); 
+            //sound effect of magnet
+            SoundManagerController.PlaySoundEffect("magnet"); 
+
+            print("hello"); 
         }
         else if(collision.gameObject.TryGetComponent<Block>(out Block block2))
         {
