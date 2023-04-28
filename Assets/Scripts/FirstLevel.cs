d38926adaf6a32c90c274ad5c60f0c8a8c2e7041
SerlyTeymoorian <teymoorian@cpp.edu>
2023-04-28 12:37:27 -0700

    switch to level 1 after level 0

---
+++ Assets/Scripts/FirstLevel.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FirstLevel : MonoBehaviour
+{
+    public float changeTime; 
+    void Update()
+    {
+        changeTime -= Time.deltaTime;
+
+        if(changeTime <= 0)
+        {
+            SceneManager.LoadScene("Level1"); 
+        }
+    }
+}
