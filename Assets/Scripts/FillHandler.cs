54ec4c30ad4c8f745169a6143777fa11f3678ea6
SerlyTeymoorian <teymoorian@cpp.edu>
2023-05-04 15:22:47 -0700

    comments of new scripts added

--- Assets/Scripts/FillHandler.cs
+++ Assets/Scripts/FillHandler.cs
@@ -1,14 +1,27 @@
+/***************************************************************
+*file: FillHandler.cs
+*author: Group
+*class: CS 4700- Game Development
+*assignment: Program 4
+*date last modified: 5/04/2023
+*
+*purpose:  This script is responsible for handling the fill part 
+*of the volume slider. 
+*
+****************************************************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FillHandler : MonoBehaviour
 {
+    //get the rect transform component of fill game object 
     public RectTransform fill; 
 
     // Start is called before the first frame update
     void Start()
     {
+        //fill the volume slider to its max 
         fill.anchorMax  = new Vector2(1, 1); 
     }
 }
--- Assets/Scripts/HandleknobController.cs
+++ Assets/Scripts/HandleknobController.cs
@@ -1,3 +1,14 @@
+/***************************************************************
+*file: HandleknobController.cs
+*author: Group
+*class: CS 4700- Game Development
+*assignment: Program 4
+*date last modified: 5/04/2023
+*
+*purpose:  This script is responsible for handling the handle knob 
+*of the volume slider. 
+*
+****************************************************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +21,7 @@ public class HandleknobController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //adjust the min and max so the knob is at the max position 
        handleKnob.anchorMax = new Vector2(1f, 1f); 
        handleKnob.anchorMin = new Vector2(0, 1f); 
     }
